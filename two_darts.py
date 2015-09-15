4f3b455dbbdbec47aef774eb868c1d2a633c730e
oli cairns <olicairns93@gmail.com>
2015-09-15 16:18:05 +0100

    refactored and reorganised, commented first 1/2 of program

--- two_darts.py
+++ two_darts.py
@@ -1,131 +1,141 @@
 from IPython import embed
 from math import log10
+
+# 'discount' by 'd' for every 3 darts thrown in order to evalaute relative speen of paths
 d = 0.95
 
-# params
-pdd = 0.45
-pds = 0.325
-ptt = 0.4
-pbb = 0.40
-pbob = 0.4
+# how accurate is our player?
+accuracy = {
+'pdd': 0.45,
+'pds' : 0.325,
+'ptt' : 0.4,
+'pbb' : 0.40,
+'pbob' : 0.4
+}
 
-pdm = 1 - pdd - pds
-pts = 1 - ptt
-pbs = (1 - pbb -pbob) / 20
+def find_optimal_strategies(accuracy):
+	# data structure - 3 lists, one for 1st, 2nd and 3rd throw - within list, index = score
+	optimal_strategies = [[],[],[]]
 
-board = range(1,21)
-current_scores = range(0,502)
-optimal_strategies = [[],[],[]]
+	# calculating remaining accuracy parameters
+	accuracy['pdm'] = 1 - accuracy['pdd'] - accuracy['pds']
+	accuracy['pts'] = 1 - accuracy['ptt']
+	accuracy['pbs'] = (1 - accuracy['pbb'] -accuracy['pbob']) / 20
 
-# set expectation of winning = 1 and getting stuck on 1 to 0
-for num in range(0,3):
-	optimal_strategies[num].append({'strategy':'game finished','expectation':1})
-	optimal_strategies[num].append({'strategy':'N/A','expectation':0})
+	# 0 = won, 1 = bust!
+	for num in range(0,3):
+		optimal_strategies[num].append({'strategy':'game finished','expectation':1})
+		optimal_strategies[num].append({'strategy':'N/A','expectation':0})
 
-a = pdd*(1 + pdm + (pdm*pdm))
-b = 1 - (d*(pds + (pdm*pds) + (pdm*pdm*pds) + (pdm*pdm*pdm)))
-e_2_1 = a / b
-e_2_3 = pdd + d*(pdm+pds)*e_2_1
-e_2_2 = pdd + (pdm*e_2_3) + (d*pds*e_2_1)
+	# solutions to similtaneous equations for strategy of double 1 on score 2 (only strategy that doesn't go bust!)
+	e_2_1_denominator = 1 - (d*(accuracy['pds'] + (accuracy['pdm']*accuracy['pds']) + (accuracy['pdm']*accuracy['pdm']*accuracy['pds']) + (accuracy['pdm']*accuracy['pdm']*accuracy['pdm'])))
+	e_2_1 = (accuracy['pdd']*(1 + accuracy['pdm'] + (accuracy['pdm']*accuracy['pdm'])))/e_2_1_denominator
+	e_2_3 = accuracy['pdd'] + d*(accuracy['pdm']+accuracy['pds'])*e_2_1
+	e_2_2 = accuracy['pdd'] + (accuracy['pdm']*e_2_3) + (d*accuracy['pds']*e_2_1)
+	optimal_strategies[0].append({'strategy':'double one','expectation':e_2_1})
+	optimal_strategies[1].append({'strategy':'double one','expectation':e_2_2})
+	optimal_strategies[2].append({'strategy':'double one','expectation':e_2_3})
+	# start from lowest scores and work up
+	for score in range(3,502):
+		# start from 3rd throw and work backwards (so missing board and staying on same score can be tackled)
+		for current_throw in range(3,0,-1):
+			optimal_strategy = find_optimal_strategy(score,current_throw,optimal_strategies)
+			optimal_strategies[current_throw-1].append(optimal_strategy)
+	return optimal_strategies
 
-optimal_strategies[0].append({'strategy':'double one','expectation':e_2_1})
-optimal_strategies[1].append({'strategy':'double one','expectation':e_2_2})
-optimal_strategies[2].append({'strategy':'double one','expectation':e_2_3})
+def find_optimal_strategy(score,current_throw,optimal_strategies):
+	# find all strategies and expectations for given score/throw input
+	all_strategies = evaluate_all_singles(score,current_throw,optimal_strategies) + evaluate_all_doubles(score,current_throw,optimal_strategies) + evaluate_all_triples(score,current_throw,optimal_strategies)+ evaluate_bull(score,current_throw,optimal_strategies)
+	# return strategy and expectation for highest strategy
+	optimal = {'strategy':'placeholder','expectation':0}
+	for strategy in all_strategies:
+		if strategy['expectation']> optimal['expectation']:
+			optimal = strategy
+	return optimal
 
-def calculate_discount_and_next_dart(current_throw):
+# what index shall we use to look up expectations? do we need to discount?
+def generate_next_throw_dict(current_throw):
 	discount = 1
 	if(current_throw == 3):
 		current_throw = 0
 		discount *= d
 	return {'discount':discount,'next_throw_index':current_throw}
 
-def evaluate_all_singles(current_score,current_throw):
-	next_dart_dict = calculate_discount_and_next_dart(current_throw)
+def evaluate_all_singles(score,current_throw,optimal_strategies):
+	next_throw_dict = generate_next_throw_dict(current_throw)
 	possible_single_strategies = []
-	for number in range(1,21):
-		if(current_score - number >  1):
-			strategy_expectation = (next_dart_dict['discount'])*optimal_strategies[next_dart_dict['next_throw_index']][current_score-number]['expectation']
-			possible_single_strategies.append({'strategy': 'single ' +str(number), 'expectation': strategy_expectation})
-
+	for target in range(1,21):
+		if(score - target >  1):
+			strategy_expectation = (next_throw_dict['discount'])*optimal_strategies[next_throw_dict['next_throw_index']][score-target]['expectation']
+			possible_single_strategies.append({'strategy': 'single ' +str(target), 'expectation': strategy_expectation})
 	return possible_single_strategies
 
-def evaluate_non_third_doubles(current_score, current_throw):
-	possible_double_strategies = []
-	for number in range(1,21):
-		if(current_score - 2*number > -1 and current_score - 2* number != 1):
-			strategy_expectation =(
-			(pdd * optimal_strategies[current_throw][current_score - 2*number]['expectation'])+
-			(pds * optimal_strategies[current_throw][current_score- number]['expectation'])+
-			(pdm * optimal_strategies[current_throw][current_score]['expectation'])
-			)
-			possible_double_strategies.append({'strategy': 'double ' +str(number), 'expectation': strategy_expectation})
-	return possible_double_strategies
-
-def evaluate_third_double(current_score):
-	possible_double_strategies = []
-	for number in range(1,21):
-		if(current_score - 2*number > -1 and current_score - 2* number != 1):
-			z =(
-			(pdd* optimal_strategies[0][current_score - 2*number]['expectation'])+
-			(pds* optimal_strategies[0][current_score -number]['expectation'])+
-			(pdm*pdd*optimal_strategies[1][current_score - 2*number]['expectation'])+
-			(pdm*pds*optimal_strategies[1][current_score -number]['expectation'])+
-			(pdm*pdm*pdd*optimal_strategies[2][current_score - 2*number]['expectation'])+
-			(pdm*pdm*pds*optimal_strategies[2][current_score -number]['expectation'])
-			)
-			strategy_expectation = (d * z) / (1 - (d*pdm*pdm*pdm))
-			possible_double_strategies.append({'strategy': 'double ' +str(number), 'expectation': strategy_expectation})
-	return possible_double_strategies
-
-def evaluate_all_doubles(current_score, current_throw):
+def evaluate_all_doubles(score, current_throw,optimal_strategies):
 	if(current_throw == 3):
-		return evaluate_third_double(current_score)
+		return evaluate_third_doubles(score, optimal_strategies)
 	else:
-		return evaluate_non_third_doubles(current_score, current_throw)
+		return evaluate_non_third_doubles(score, current_throw,optimal_strategies)
 
-def evaluate_all_triples(current_score, current_throw):
-	next_dart_dict = calculate_discount_and_next_dart(current_throw)
+def evaluate_all_triples(score, current_throw,optimal_strategies):
+	next_throw_dict = generate_next_throw_dict(current_throw)
 	possible_triple_strategies = []
-	for number in range(1,21):
-		if(current_score - 3*number > 1):
-			strategy_expectation = (next_dart_dict['discount'])*((ptt * optimal_strategies[next_dart_dict['next_throw_index']][current_score-3*number]['expectation'])+(pts*optimal_strategies[next_dart_dict['next_throw_index']][current_score-number]['expectation']))
-			possible_triple_strategies.append({'strategy': 'triple ' +str(number), 'expectation': strategy_expectation})
+	for target in range(1,21):
+		if(score - 3*target > 1):
+			strategy_expectation = (next_throw_dict['discount'])*((accuracy['ptt'] * optimal_strategies[next_throw_dict['next_throw_index']][score-3*target]['expectation'])+(accuracy['pts']*optimal_strategies[next_throw_dict['next_throw_index']][score-target]['expectation']))
+			possible_triple_strategies.append({'strategy': 'triple ' +str(target), 'expectation': strategy_expectation})
 	return possible_triple_strategies
 
-def evaluate_bull(current_score,current_throw):
-	next_dart_dict = calculate_discount_and_next_dart(current_throw)
-	if(current_score < 50 or current_score == 51):
+def evaluate_bull(score,current_throw,optimal_strategies):
+	next_throw_dict = generate_next_throw_dict(current_throw)
+	if(score < 50 or score == 51):
 		return []
 	else:
-		strategy_expectation = (pbb * optimal_strategies[next_dart_dict['next_throw_index']][current_score-50]['expectation']) + (pbob * optimal_strategies[next_dart_dict['next_throw_index']][current_score-25]['expectation'])
+		strategy_expectation = (accuracy['pbb'] * optimal_strategies[next_throw_dict['next_throw_index']][score-50]['expectation']) + (accuracy['pbob'] * optimal_strategies[next_throw_dict['next_throw_index']][score-25]['expectation'])
 		for number in range(1,21):
-			strategy_expectation += pbs*optimal_strategies[next_dart_dict['next_throw_index']][current_score-number]['expectation']
-		strategy_expectation*=(next_dart_dict['discount'])
+			strategy_expectation += accuracy['pbs']*optimal_strategies[next_throw_dict['next_throw_index']][score-number]['expectation']
+		strategy_expectation*=(next_throw_dict['discount'])
 		return [{'strategy':'bullseye','expectation':strategy_expectation}]
 
-def find_best_strategy(current_score,throw):
-	all_strategies = evaluate_all_singles(current_score,throw) + evaluate_all_doubles(current_score,throw) + evaluate_all_triples(current_score,throw)+ evaluate_bull(current_score,throw)
-	max_strategy = {'strategy':'placeholder','expectation':0}
-	for strategy in all_strategies:
-		if strategy['expectation']> max_strategy['expectation']:
-			max_strategy = strategy
-	return max_strategy
+def evaluate_non_third_doubles(score, current_throw,optimal_strategies):
+	possible_double_strategies = []
+	for target in range(1,21):
+		if(score - 2*target > -1 and score - 2* target != 1):
+			strategy_expectation =(
+			(accuracy['pdd'] * optimal_strategies[current_throw][score - 2*target]['expectation'])+
+			(accuracy['pds'] * optimal_strategies[current_throw][score- target]['expectation'])+
+			(accuracy['pdm'] * optimal_strategies[current_throw][score]['expectation'])
+			)
+			possible_double_strategies.append({'strategy': 'double ' +str(target), 'expectation': strategy_expectation})
+	return possible_double_strategies
 
-def find_all_best_strategies(optimal_strategies):
-	for score in range(3,502):
-		for throw in range(3,0,-1):
-			best_strategy = find_best_strategy(score,throw)
-			optimal_strategies[throw-1].append(best_strategy)
-
-def make_recommendation(current_score, current_throw, optimal_strategies):
-	print optimal_strategies[current_throw-1][current_score]['strategy']
-	current_score_expectation=optimal_strategies[current_throw-1][current_score]['expectation']
-	time = convert_expectation_to_avg_time(current_score_expectation,d)
-	print "expected time left: " + str(time)
+def evaluate_third_doubles(score,optimal_strategies):
+	possible_double_strategies = []
+	for target in range(1,21):
+		if(score - 2*target > -1 and score - 2* target != 1):
+			z =(
+			(accuracy['pdd']* optimal_strategies[0][score - 2*target]['expectation'])+
+			(accuracy['pds']* optimal_strategies[0][score -target]['expectation'])+
+			(accuracy['pdm']*accuracy['pdd']*optimal_strategies[1][score - 2*target]['expectation'])+
+			(accuracy['pdm']*accuracy['pds']*optimal_strategies[1][score -target]['expectation'])+
+			(accuracy['pdm']*accuracy['pdm']*accuracy['pdd']*optimal_strategies[2][score - 2*target]['expectation'])+
+			(accuracy['pdm']*accuracy['pdm']*accuracy['pds']*optimal_strategies[2][score -target]['expectation'])
+			)
+			strategy_expectation = (d * z) / (1 - (d*accuracy['pdm']*accuracy['pdm']*accuracy['pdm']))
+			possible_double_strategies.append({'strategy': 'double ' +str(target), 'expectation': strategy_expectation})
+	return possible_double_strategies
 
 def convert_expectation_to_avg_time(expectation,d):
 	ans = log10(expectation)/log10(d)
 	return round(ans,2)
+
+# methods not used in 'find optimal strategies'
+
+def make_recommendation(score, current_throw, optimal_strategies):
+	print optimal_strategies[current_throw-1][score]['strategy']
+	score_expectation=optimal_strategies[current_throw-1][score]['expectation']
+	time = convert_expectation_to_avg_time(score_expectation,d)
+	print "expected time left: " + str(time)
+
 
 def calculate_player_average(optimal_strategies):
 	starting_expectation = optimal_strategies[0][501]['expectation']
@@ -148,8 +158,6 @@ def print_strategy_table(optimal_strategies):
 			score_count +=1
 		throw_count += 1
 
-find_all_best_strategies(optimal_strategies)
-
-
+optimal_strategies = find_optimal_strategies(accuracy)
 print_strategy_table(optimal_strategies)
 calculate_player_average(optimal_strategies)
